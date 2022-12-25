e449ec5fc8f06fc84bece5a9dace2a317b8e8349
Kalpesh Ketkar <106992078+KetkarKalpesh@users.noreply.github.com>
2022-12-25 15:05:18 +0530

    Communication using Custom Event

--- childToParentCustomEventParent/childToParentCustomEventParent.js
+++ childToParentCustomEventParent/childToParentCustomEventParent.js
@@ -2,13 +2,15 @@ import { LightningElement } from 'lwc';
 
 export default class ChildToParentCustomEventParent extends LightningElement {
     showData = false
+    msg
 
     handleClick(){
         //console.log(this.showData)
         this.showData = true
     }
 
-    closeHandler(){
+    closeHandler(event){
+        this.msg = event.detail.msg
         this.showData = false
     }
 }
