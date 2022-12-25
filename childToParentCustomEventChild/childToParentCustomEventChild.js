11ad65b1ed0ac1501900e8fbc82e2f627d3908d7
Kalpesh Ketkar <106992078+KetkarKalpesh@users.noreply.github.com>
2022-12-25 15:04:55 +0530

    Communication using Custom Event

--- childToParentCustomEventChild/childToParentCustomEventChild.js
+++ childToParentCustomEventChild/childToParentCustomEventChild.js
@@ -2,7 +2,11 @@ import { LightningElement } from 'lwc';
 
 export default class ChildToParentCustomEventChild extends LightningElement {
     closeHandle(){
-        const closeEvent = new CustomEvent('close')
+        const closeEvent = new CustomEvent('close', {
+            detail: {
+                msg: "Component Closed Succesfully!!!"
+            }
+        })
         this.dispatchEvent(closeEvent)
     }
 }
